b1566322f56d3b4efd35ea2b5dac9e87c59c6c51
stig <quanghung291004@gmail.com>
2026-03-23 16:12:01 +0700

    feat: Implement backend for film search and filter (#66)

--- backend/src/main/java/com/starview/cinemabooking/repository/PhimRepository.java
+++ backend/src/main/java/com/starview/cinemabooking/repository/PhimRepository.java
@@ -3,9 +3,10 @@ package com.starview.cinemabooking.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.starview.cinemabooking.model.Phim;
 
-public interface PhimRepository extends JpaRepository<Phim, Integer>{
+public interface PhimRepository extends JpaRepository<Phim, Integer>, JpaSpecificationExecutor<Phim> {
 	List<Phim> findByIsActiveTrue();
 }
